6a25a1f4ff27444648fb8764c8c9434c3b896e6c
rex10naruto <38184817+rex10naruto@users.noreply.github.com>
2024-09-23 17:27:30 +0000

    Added dynamic data and api route

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask,render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+}]
+#we ca also use API and get the data from it and then pass it to the html page and we  can use json
+
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(
+        JOBS
+    )  #we send the data in json format and insted of using render_template we use jsonify and differenciate htmlpages and non html pages we often use api name infront of it. it bassically means url which  don't return html in the broweser but it return stuctured data in json format which can then ananlyzed and now we have made api route and now we wiil deploy it in render.com.
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
-if __name__=="__main__":
-    app.run(host='0.0.0.0',debug=True)
+    return render_template('home.html', jobs=JOBS, company_name='Jovian')
 
-  
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
